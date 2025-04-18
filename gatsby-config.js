39292d0f3137b375e9ebea8b3cdb498ffadd135c
MataRocksbze <matarocksbzresort@outlook.com>
2025-04-18 16:42:21 -0600

    site updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-sitemap`,
+    'gatsby-plugin-netlify',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
